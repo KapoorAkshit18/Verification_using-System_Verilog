4d5a82398073022876aec57be43b62b8b4409062
Akshit Kapoor <98168037+KapoorAkshit18@users.noreply.github.com>
2025-11-03 14:33:34 +0530

    Update full_adder_tb.sv
    added function and class

--- Project_1/full_adder_tb.sv
+++ Project_1/full_adder_tb.sv
@@ -6,4 +6,17 @@ class txn;
 rand bit a,b,cin;
 bit sum_tmp, cout_tmp;
 
-function 
+function check_fa ();
+assign temp = a+b+cin; //why temp
+temp[1]=cout_tmp;
+temp[0]=sum_tmp;
+assert(sum_tmp==sum && cout_tmp==cout);
+else
+$display("error the results are not correct");
+endfunction 
+endclass
+
+module fa_tb;
+logic a,b,cin;
+logic sum,cout;
+
