e2733325a13fbceaae163ad0f734f01b2ca806cf
Akshit Kapoor <98168037+KapoorAkshit18@users.noreply.github.com>
2025-11-07 15:21:27 +0530

    Improve full adder testbench structure and assertions
    Debugged for the errors.

--- Project_1/full_adder_tb.sv
+++ Project_1/full_adder_tb.sv
@@ -8,14 +8,18 @@ bit sum_tmp, cout_tmp;
 
 function check_fa ();
 bit[1:0] temp;
-assign temp = a+b+cin; //why temp
+temp = a+b+cin; //why temp
 temp[1]=cout_tmp;
 temp[0]=sum_tmp;
-assert(sum_tmp==sum && cout_tmp==cout);
+/*assert(sum_tmp==sum && cout_tmp==cout);
 else
-$display("error the results are not correct");
+$display("error the results are not correct"); */
 endfunction 
 endclass
+
+//-------------------------------------------------
+// Testbench Module
+//-------------------------------------------------  
 
 module fa_tb;
 
@@ -33,19 +37,31 @@ fa dut (
 
 txn t;
 
-initial 
-begin 
+initial begin 
+  t=new();
+  
+  repeat(10) begin
+            assert(t.randomize());      // Randomize Inputs
+    t.a = a;  t.b = b;  t.cin = cin;      // Drive DUT
 
-t=new();
-repeat(10);
-assert(t.randomize());
-t.check_fa
-t.a=a;t.b=b; t.cin=cin;
+    #1;
+          
+    t.check_fa();
+
+// Check Results
+
+            if (sum !== t.sum_tmp || cout !== t.cout_tmp)
+        $display("❌ Error: a=%0b b=%0b cin=%0b | sum=%0b (exp=%0b) cout=%0b (exp=%0b)",
+                  t.a, t.b, t.cin, sum, t.sum_tmp, cout, t.cout_tmp);
+      else
+        $display("✅ Pass: a=%0b b=%0b cin=%0b | sum=%0b cout=%0b",
+                  t.a, t.b, t.cin, sum, cout);
 
 
 
 
-end
+  end
+ end
 endmodule
 
 
